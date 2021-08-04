c954001af35bad50b29691b7b68d252826dbc0a7
ComPilErr <compilerr@ya.ru>
2021-08-04 12:25:49 +0300

    Create uci_puci.py
    add uci class

---
+++ uci_puci.py
@@ -0,0 +1,38 @@
+import sys
+import subprocess
+
+
+class UciPuci:
+
+    def __init__(self, moves = ""):
+        self.moves = moves
+        self.p = subprocess.Popen("../StockFish/src/stockfish",stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+
+        self.p.stdin.write("uci\n")
+        self.p.stdin.write("option name Hash type spin default 1 min 1 max 128\n")
+        self.p.stdin.write("option name Nullmove type check default true\n")
+        self.p.stdin.write("option name Style type combo default Normal var Solid var Normal var Risky\n")
+        self.p.stdin.write("setoption name Hash value 32\n")
+        self.p.stdin.write("isready\n")
+
+    def __del__(self):
+        self.p.terminate()
+
+    def process(self):
+        self.p.stdin.write("ucinewgame\n")
+
+        if self.moves is not "":
+            self.p.stdin.write("position startpos moves "+self.moves+"\n")
+        else:
+            self.p.stdin.write("position startpos moves\n")
+        self.p.stdin.write("go movetime 2000\n")
+        out = ""
+        for _ in iter(self.p.stdout.readline,''):
+            pass
+            if 'bestmove' in _:
+                out = _
+                break
+        return(out.split(' ')[1])
+
+    def move(self, moves):
+        self.moves += moves
